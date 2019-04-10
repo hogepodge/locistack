4837abdf1928aec52b7bd45f5d0d6576e06918fd
Chris Hoge <chris@openstack.org>
2019-04-10 00:34:51 -0400

    Keystone and catalog creation are unreliable. Refactoring
    Keystone catalog creation across the clients is unreliable.
    Falling back to a central catalog creation in Keystone to
    improve network reliability by reducing parallelism and network
    proxy drops.

--- deploy/scripts/common/initialize-keystone.sh
+++ deploy/scripts/common/initialize-keystone.sh
@@ -9,13 +9,9 @@ PUBLIC_ENDPOINT="$5"
 PRIVATE_ENDPOINT="$6"
 ADMIN_ENDPOINT="$7"
 
-OPENSTACK="openstack --insecure"
+OPENSTACK=openstack
 
-until $(curl --output /dev/null --silent --head --fail --insecure https://${CONTROL_HOST_IP}:5000); do
-    printf '.'
-    sleep 5
-done
-
+/scripts/common/wait-for-service.sh Keystone 5000
 
 function create_service_user() {
     local SERVICE_NAME="$1"
@@ -24,8 +20,7 @@ function create_service_user() {
 
     ${OPENSTACK} user show ${SERVICE_NAME}
 
-    if [ $? -eq 1 ]
-    then
+    if [ $? -eq 1 ]; then
         ${OPENSTACK} user create --password ${SERVICE_PASSWORD} ${SERVICE_NAME}
         ${OPENSTACK} role add --user ${SERVICE_NAME} --project service admin
     fi
@@ -37,10 +32,9 @@ function create_service() {
   local SERVICE_DESCRIPTION="$3"
   local OPENSTACK="$4"
 
-  ${OPENSTACK} service show ${SERVICE_NAME}
+  ${OPENSTACK} service list | grep ${SERVICE_NAME}
 
-  if [ $? -eq 1 ]
-  then
+  if [ $? -eq 1 ]; then
       ${OPENSTACK} service create --name ${SERVICE_NAME} \
                                           --description "${SERVICE_DESCRIPTION}" \
                                           ${SERVICE_TYPE}
@@ -54,18 +48,17 @@ function create_service_endpoint() {
   local REGION="$4"
   local OPENSTACK="$5"
 
-  ${OPENSTACK} endpoint list | \
-      grep ${REGION} | \
-      grep ${SERVICE_TYPE} | \
-      grep ${ENDPOINT_TYPE} | \
-      grep ${ENDPOINT}
+  ${OPENSTACK} endpoint list |
+      grep "${REGION}" |
+      grep "${SERVICE_TYPE}" |
+      grep "${ENDPOINT_TYPE}" |
+      grep "${ENDPOINT}"
 
-  if [ $? -eq 1 ]
-  then
-      ${OPENSTACK} endpoint create --region ${REGION} \
-                                          ${SERVICE_TYPE} \
-                                          ${ENDPOINT_TYPE} \
-                                          ${ENDPOINT}
+  if [ $? -eq 1 ]; then
+      ${OPENSTACK} endpoint create --region "${REGION}" \
+                                          "${SERVICE_TYPE}" \
+                                          "${ENDPOINT_TYPE}" \
+                                          "${ENDPOINT}"
   fi
 }
 
@@ -97,7 +90,7 @@ function initialize_service() {
 export OS_USERNAME=admin
 export OS_PASSWORD=${KEYSTONE_ADMIN_PASSWORD}
 export OS_TENANT_NAME=admin
-export OS_AUTH_URL=https://${CONTROL_HOST_IP}:5000/v3
+export OS_AUTH_URL=http://${CONTROL_HOST_IP}:5000/v3
 export OS_REGION_NAME=RegionOne
 export OS_IDENTITY_API_VERSION=3
 
--- deploy/scripts/keystone/start-keystone-api.sh
+++ deploy/scripts/keystone/start-keystone-api.sh
@@ -23,12 +23,38 @@ keystone-manage db_sync
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 keystone-manage bootstrap --bootstrap-password $KEYSTONE_ADMIN_PASSWORD \
-  --bootstrap-admin-url https://${CONTROL_HOST_IP}:35357/v3/ \
-  --bootstrap-internal-url https://${CONTROL_HOST_IP}:5000/v3/ \
-  --bootstrap-public-url https://${CONTROL_HOST_IP}:5000/v3/ \
+  --bootstrap-admin-url http://${CONTROL_HOST_IP}:35357/v3/ \
+  --bootstrap-internal-url http://${CONTROL_HOST_IP}:5000/v3/ \
+  --bootstrap-public-url http://${CONTROL_HOST_IP}:5000/v3/ \
   --bootstrap-region-id RegionOne
 
+
 # Start uwsgi
-uwsgi --uid 42424 --gid 42424 --https :35357,/tls/openstack.crt,/tls/openstack.key --wsgi-file /var/lib/openstack/bin/keystone-wsgi-admin &
-uwsgi --uid 42424 --gid 42424 --https :5000,/tls/openstack.crt,/tls/openstack.key --wsgi-file /var/lib/openstack/bin/keystone-wsgi-public &
-wait %1 %2
+uwsgi --uid 42424 \
+      --gid 42424 \
+      --http :35357 \
+      --listen 1024 \
+      --workers 1 \
+      --thunder-lock \
+      --enable-threads \
+      --buffer-size 65535 \
+      --wsgi-file /var/lib/openstack/bin/keystone-wsgi-admin &
+
+uwsgi --uid 42424 \
+      --gid 42424 \
+      --http :5000 \
+      --listen 1024 \
+      --workers 1 \
+      --thunder-lock \
+      --enable-threads \
+      --buffer-size 65535 \
+      --wsgi-file /var/lib/openstack/bin/keystone-wsgi-public &
+
+ONE=%1
+TWO=%2
+
+echo "creating endpoints"
+/scripts/keystone/initialize-endpoints.sh > endpoint_log 2>&1
+cat endpoint_log
+
+wait $ONE $TWO
